9bfee275b17160dd921a7114dc82edc94cf69e63
Juwel-771 <juwelah99@gmail.com>
2025-05-22 22:57:26 +0600

    total score created

--- src/components/GamePlay.jsx
+++ src/components/GamePlay.jsx
@@ -1,13 +1,25 @@
 import React from 'react';
 import TotalScore from './TotalScore';
 import NumberSelector from './NumberSelector';
+import styled from 'styled-components';
+
+const MainContainer = styled.main`
+    .top-section {
+        display: flex;
+        justify-content: space-around;
+        align-items: end;
+    }
+`
 
 const GamePlay = () => {
     return (
-        <main>
-            <TotalScore></TotalScore>
-            <NumberSelector></NumberSelector>
-        </main>
+        <MainContainer>
+            <div className='top-section'>
+                <TotalScore></TotalScore>
+                <NumberSelector></NumberSelector>
+            </div>
+            
+        </MainContainer>
     );
 };
 
--- src/components/NumberSelector.jsx
+++ src/components/NumberSelector.jsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
+import './NumberSelector.css'
 
 const Box = styled.div`
     width: 72px;
@@ -9,17 +10,49 @@ const Box = styled.div`
     place-items: center;
     font-size: 24px;
     font-weight: 700;
+    cursor: pointer;
+
+    background-color: ${(props)=> (props.isSelected ? "black" : "white")};
+    color: ${(props)=> (!props.isSelected ? "black" : "white")};
+`
+
+const NumberSelectorContainer = styled.div `
+
+    display: flex;
+    flex-direction: column;
+    align-items: end;
+
+    .flex {
+        display: flex;
+        gap: 24px;
+    }
+    p {
+        font-size: 24px;
+    }
 `
 
 const arrayNumber = [1,2,3,4,5,6];
 
 const NumberSelector = () => {
+
+    const [selectedNumber, setSelectedNumber] = useState(0);
+
     return (
-        <div>
+        <NumberSelectorContainer>
+            <div className='flex'>
             {
-                arrayNumber.map((value, idx) => <Box key={idx}>{value}</Box>)
+                arrayNumber.map((value, idx) => 
+                <Box
+                    isSelected = {value == selectedNumber} 
+                    key={idx} 
+                    onClick={()=>setSelectedNumber(value)
+                    } >
+                        {value}
+                </Box>)
             }
         </div>
+            <p>Select Number</p>
+        </NumberSelectorContainer>
     );
 };
 
